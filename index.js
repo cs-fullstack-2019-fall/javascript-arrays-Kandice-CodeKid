9dc26016971c68da8c9bf164c43168b2465df7ea
Kandice CodeKid <you@example.com>
2019-08-13 12:12:29 -0500

    08/13/2019 morning assignment

---
+++ index.js
@@ -0,0 +1,52 @@
+alert("Test");
+
+// #### Exercise 1
+// 1. Create an array.
+// 2. Add four names to the array.
+// 3. Print the array
+
+// let folksNames=["Jerry","Kamille","Marcus","Marvin"];
+// function alertNames(namesInIrray)
+
+// console.log(folksNames[0]);
+// console.log(folksNames[1]);
+// console.log(folksNames[2]);
+// console.log(folksNames[3]);
+
+
+
+
+
+//
+// #### Exercise 2
+// 1. Create an array with 5 numbers in it.
+// 2. Print the numbers.
+
+// let theNumbers=[10,20,30,40,50];
+// console.log(theNumbers[0]);
+// console.log(theNumbers[1]);
+// console.log(theNumbers[2]);
+// console.log(theNumbers[3]);
+// console.log(theNumbers[4]);
+
+
+//
+//     #### Exercise 3
+// 1. Create an array with 4 names.
+// 2. Print the third item in the array.
+// 3. Delete the second item.
+// 4. Print the third item in the array again.
+
+let folksNames2=["Antonio","Arami","Phaith","Jordan"];
+
+console.log(folksNames2[2]);
+folksNames2.shift("Antonio");
+folksNames2.shift("Arami");
+folksNames2.unshift("Antonio");
+console.log(folksNames2[2]);
+
+// console.log.unshift([0]);
+
+// console.log(folksNames2[1]);
+// console.log(folksNames2[2]);
+// console.log(folksNames2[3]);
